f6e10e04de8488ed9e1f550a1899a176f97640d9
Amro <amro.youssef@ymail.com>
2026-03-05 10:45:20 +0000

    Make navbar stay at top of page even if page is empty

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import NotFound from "./pages/NotFound/NotFound";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/NavBar/NavBar";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+    return(
+        <div style={{ padding: "2rem" }}>
+            hello
+            hello
+        </div>
+    )
+}
+
+export default Signup
